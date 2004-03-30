be35370a5b3a116929252c3a9a08379ab45ca233
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2004-03-30 11:07:46 +0000

    Just a minor style issue..

--- SQLSource.py
+++ SQLSource.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.10 $
+# $Revision: 1.11 $
 from interfaces import IExternalSource
 from ExternalSource import ExternalSource
 # Zope
@@ -70,7 +70,7 @@ class SQLSource(ExternalSource, Folder):
         # XXX: we're materializing all data in the resultset here which
         # hurts performance for large sets. Brains 'normally' lazily
         # retrieve the data, but then we need to decode to unicode in
-        # the wrong - i.e. in the ZPT layer - layer...
+        # the wrong - i.e. in the ZPT's - layer...
         #
         # We need a better way.
         #

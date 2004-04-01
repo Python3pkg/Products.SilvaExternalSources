66b0b48177f576675102a978c85eac9a749fe01b
Infrae <info@infrae.com>
2004-04-01 16:11:48 +0000

    Added view and meta-data registration for CSVSource.

--- install.py
+++ install.py
@@ -1,5 +1,9 @@
 """Install for Silva External Sources extension
 """
+
+from Products.Silva.install import add_fss_directory_view
+
+
 def is_installed(root):
     # Hack to get installed state of this extension
     return hasattr(root.service_views, 'SilvaExternalSources')
@@ -7,7 +11,43 @@ def is_installed(root):
 def install(root):
     # Hack - refresh SilvaDocument to make it pick up this extension
     root.service_extensions.refresh('SilvaDocument')
-    root.service_views.manage_addFolder('SilvaExternalSources')
+    add_fss_directory_view(root.service_views, 'SilvaExternalSources', __file__, 'views')
+    # also register views
+    registerViews(root.service_view_registry)
+    # metadata registration
+    setupMetadata(root)
 
 def uninstall(root):
+    unregisterViews(root.service_view_registry)
     root.service_views.manage_delObjects(['SilvaExternalSources'])
+
+
+def registerViews(reg):
+    """Register core views on registry.
+    """
+    # add
+    reg.register('add', 'Silva CSV Source', ['add', 'CSVSource'])
+    # edit
+    reg.register('edit', 'Silva CSV Source', ['edit', 'Asset', 'CSVSource'])
+    # public
+    reg.register('public', 'Silva CSV Source', ['public', 'CSVSource'])
+
+def unregisterViews(reg):
+    """Unregister core views on registry.
+    """
+    # add
+    reg.unregister('add', 'Silva CSV Source')
+    # edit
+    reg.unregister('edit', 'Silva CSV Source')
+    # public
+    reg.unregister('public', 'Silva CSV Source')
+
+def setupMetadata(root):
+    mapping = root.service_metadata.getTypeMapping()
+    default = ''
+    tm = (
+            {'type': 'Silva CSV Source', 'chain': 'silva-content, silva-extra'},
+        )
+    mapping.editMappings(default, tm)
+
+

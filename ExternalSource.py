a9d8c3fabf0c369d94093e509a3f22298c7a2637
Infrae <info@infrae.com>
2004-04-07 14:54:22 +0000

    - changed default charset from 'ascii' to 'latin1'

--- ExternalSource.py
+++ ExternalSource.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.7 $
+# $Revision: 1.8 $
 from interfaces import IExternalSource
 # Zope
 import Acquisition
@@ -78,7 +78,7 @@ class ExternalSource(Acquisition.Implicit):
     security = ClassSecurityInfo()
 
     parameters = None # Cannot make it 'private'; the form won't work in the ZMI if it was.
-    _data_encoding = 'ascii'
+    _data_encoding = 'latin1'
     _description = 'No description available'
     _is_cacheable = 0
 

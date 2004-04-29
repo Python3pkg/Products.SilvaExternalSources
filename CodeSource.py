a18b5492af9890ba16a26398a552031c36779847
Infrae <info@infrae.com>
2004-04-29 17:04:48 +0000

    changed copyright date

--- CodeSource.py
+++ CodeSource.py
@@ -1,6 +1,6 @@
-# Copyright (c) 2002 Infrae. All rights reserved.
+# Copyright (c) 2002-2004 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.8 $
+# $Revision: 1.9 $
 from interfaces import IExternalSource
 from ExternalSource import ExternalSource
 # Zope

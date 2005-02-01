3e749923f6e061d843074d98ebfc60be0623f315
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2005-02-01 15:10:58 +0000

    Be a bit more tolerant when unicodifying the string.

--- CodeSource.py
+++ CodeSource.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002-2005 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.15 $
+# $Revision: 1.16 $
 from interfaces import IExternalSource
 from ExternalSource import ExternalSource
 # Zope
@@ -58,7 +58,7 @@ class CodeSource(ExternalSource, Folder):
         result = script(**kw)
         if type(result) is unicode:
             return result
-        return unicode(result, self.data_encoding())
+        return unicode(result, self.data_encoding(), 'replace')
     
     # MANAGERS
 

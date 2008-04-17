e7ccb03bbf0104edd354578cf8dac980cf7af9c4
Andy Altepeter <aaltepet@altepeter.net>
2008-04-17 01:13:40 +0000

    removed containerfilter setting in __init__.py, so code and sql sources are now addable via the SMI outside a Silva root

--- __init__.py
+++ __init__.py
@@ -3,50 +3,23 @@ import CodeSource, SQLSource, CSVSource, CodeSourceService
 # Silva
 from Products.Silva.ExtensionRegistry import extensionRegistry
 
-has_makeContainerFilter = True
-try:
-    from Products.Silva.helpers import makeContainerFilter
-except:
-    has_makeContainerFilter = False
-
 def initialize(context):
-    if has_makeContainerFilter:
-        context.registerClass(
-            CodeSource.CodeSource,
-            constructors = (
-            CodeSource.addCodeSource,
-            CodeSource.manage_addCodeSource
-            ),
-            icon = "www/codesource.png",
-            container_filter = makeContainerFilter()
-            )
-        context.registerClass(
-            SQLSource.SQLSource,
-            constructors = (
-            SQLSource.addSQLSource,
-            SQLSource.manage_addSQLSource
-            ),
-            icon = "www/sqlsource.png",
-            container_filter = makeContainerFilter()        
-            )
-    else:
-        context.registerClass(
-            CodeSource.CodeSource,
-            constructors = (
-            CodeSource.addCodeSource,
-            CodeSource.manage_addCodeSource
-            ),
-            icon = "www/codesource.png"
-            )
-        context.registerClass(
-            SQLSource.SQLSource,
-            constructors = (
-            SQLSource.addSQLSource,
-            SQLSource.manage_addSQLSource
-            ),
-            icon = "www/sqlsource.png"
-            )
-
+    context.registerClass(
+        CodeSource.CodeSource,
+        constructors = (
+        CodeSource.addCodeSource,
+        CodeSource.manage_addCodeSource
+        ),
+        icon = "www/codesource.png"
+        )
+    context.registerClass(
+        SQLSource.SQLSource,
+        constructors = (
+        SQLSource.addSQLSource,
+        SQLSource.manage_addSQLSource
+        ),
+        icon = "www/sqlsource.png"
+        )
     context.registerClass(
         CodeSourceService.CodeSourceService,
         constructors = (

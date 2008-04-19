cbcceca6c6010785472a997b3f06c32a410ae8e6
Kit Blake <kitblake@infrae.com>
2008-04-19 12:41:11 +0000

    Improve descriptions

--- CodeSources/configure.py
+++ CodeSources/configure.py
@@ -1,3 +1,6 @@
+# XXX this conf needs settings for "Source is cachable" and "Source is elaborate".
+# Both cs_YouTube and cs_GoogleCalendar are cachable.
+
 configuration = {
     'cs_YouTube': {
         'id': 'cs_youtube',

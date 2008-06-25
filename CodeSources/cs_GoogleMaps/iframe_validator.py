8259faf587ad54b86c862b2f3f4094a8875b7e56
Wim Boucquaert <wim.boucquaert@gmail.com>
2008-06-25 10:23:36 +0000

    updated validation script

--- CodeSources/cs_GoogleMaps/iframe_validator.py
+++ CodeSources/cs_GoogleMaps/iframe_validator.py
@@ -17,21 +17,35 @@ def onlyFirst((number, value)):
 def clean(value):
    return value.replace(' ', '').replace('\n', '')
 
+def removeXmlData(value):
+   xml = False
+   cleaned = ''
+   for pos in range(len(value)):
+      if value[pos] == '<':
+         xml = True
+      elif value[pos] == '>':
+         xml = False
+      elif not xml:
+         continue
+      cleaned += value[pos]
+
+   return cleaned
+
 # Extract information
 cleaned = map(clean, iframe.split('"'))
 keys = map(onlyFirst, filter(notEven, enumerate(cleaned)))
 values = map(onlyFirst, filter(notOdd,  enumerate(cleaned)))
 
 # Check tags and structure
-stdigest = ''.join(keys)
-wanted_stdigest = '<iframewidth=height=frameborder=scrolling=marginheight=marginwidth=src=></iframe><br/><small><ahref=style=>ViewLargerMap</a></small>'
+stdigest = removeXmlData(''.join(keys))
+wanted_stdigest = '<iframewidth=height=frameborder=scrolling=marginheight=marginwidth=src=></iframe><br/><small><ahref=style=></a></small>'
 if stdigest != wanted_stdigest:
     return False
 
 # Check that all urls link to maps.google.com
 for key, value in zip(keys, values):
     if key.endswith('src=') or key.endswith('href='):
-        if not value.startswith('http://maps.google.com/'):
+        if not (value.startswith('http://maps.google.com/') or value.startswith('http://www.google.com/maps')):
             return False
 
 return True

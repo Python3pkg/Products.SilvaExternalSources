9a0654436c9b35d26629a8f12e22ee80c66d2984
Sylvain Viollon <thefunny@gmail.com>
2009-11-17 12:55:47 +0000

    Fix errors

--- CodeSources/cs_portlet_element/get_portlet_content.py
+++ CodeSources/cs_portlet_element/get_portlet_content.py
@@ -6,7 +6,7 @@ if show_title=='no':
   request.set('suppress_title','yes')
 
 model = request['model']
-html getattr(context.restrictedTraverse(str(document), None), view_method)()
+html = getattr(model.restrictedTraverse(str(document), None), view_method)()
 
 request.model = model
 request['model'] = model

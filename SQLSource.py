d99076040a46215b1220f1eee7d7638e6020a90a
Sylvain Viollon <thefunny@gmail.com>
2010-01-15 10:08:33 +0000

    Fix svn:props

--- SQLSource.py
+++ SQLSource.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002-2010 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.24 $
+# $Revision$
 from interfaces import IExternalSource
 from ExternalSource import ExternalSource
 from zope.interface import implements

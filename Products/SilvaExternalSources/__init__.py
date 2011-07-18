f78dc80a7c0e78eca63b87320eff293bc59e8636
Sylvain Viollon <thefunny@gmail.com>
2011-07-18 11:28:23 +0200

    Fix installation code.

--- Products/SilvaExternalSources/__init__.py
+++ Products/SilvaExternalSources/__init__.py
@@ -2,10 +2,38 @@
 # See also LICENSE.txt
 # $Id$
 
-from Products.SilvaExternalSources import install
-
 from silva.core import conf as silvaconf
+from silva.core.conf.installer import DefaultInstaller
+from zope.interface import Interface
 
 silvaconf.extension_name('SilvaExternalSources')
 silvaconf.extension_title('Silva External Sources')
 silvaconf.extension_default()
+
+
+class IExtension(Interface):
+    """Silva External Sources extension.
+    """
+
+class SilvaExternalSourcesInstaller(DefaultInstaller):
+    """Silva External Sources installer
+    """
+
+    def install_custom(self, root):
+        installed_ids = root.objectIds()
+        if 'service_codesources' not in installed_ids:
+            factory = root.manage_addProduct['SilvaExternalSources']
+            factory.manage_addCodeSourceService()
+
+        for source_id in ['cs_toc', 'cs_citation',]:
+            if source_id not in installed_ids:
+                source = root.service_codesources.get_installable_source(source_id)
+                source.install(root)
+
+
+    def uninstall_custom(self, root):
+        if 'service_codesources' in root.objectdIds():
+            root.manage_delObjects(['service_codesources'])
+
+
+installer = SilvaExternalSourcesInstaller('SilvaExternalSources', IExtension)

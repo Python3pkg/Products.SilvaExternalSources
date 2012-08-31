d97591ede9e00a529f526329395cf87a69a1273d
Sylvain Viollon <thefunny@gmail.com>
2012-08-31 10:53:56 +0200

    Fix edge case.

--- Products/SilvaExternalSources/editor/static/plugins/silvaexternalsource/plugin.js
+++ Products/SilvaExternalSources/editor/static/plugins/silvaexternalsource/plugin.js
@@ -300,9 +300,13 @@
                             if (is_source(element)) {
                                 var attributes = element.attributes;
                                 var parent = element.parent;
-                                var style = /^external-source (.*)$/.exec(
-                                    element.attributes['class'])[1];
+                                var parse_alignement = /^external-source (.*)$/.exec(
+                                    element.attributes['class']);
+                                var alignement = 'default';
 
+                                if (parse_alignement !== null) {
+                                    alignement = parse_alignement[1];
+                                };
                                 attributes['contenteditable'] = 'false';
                                 element.children = [];
                                 if (!is_container(parent)) {
